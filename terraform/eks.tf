695911bf4d7b1e5c3d3cd03e1d721e851640f97e
bhagyasri12563 <bhagyasri12563@gmail.com>
2026-03-08 13:06:15 +0530

    Update eks.tf

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -1,1 +1,11 @@
+resource "aws_eks_cluster" "cluster" {
+  name = "enterprise-catering-cluster"
+  role_arn = "arn:aws:iam::738222620717:role/EKSRole"
+  vpc_config {
+    subnet_ids = [
+      aws_subnet.public1.id,
+      aws_subnet.public2.id
+      ]
+ }
 
+}
